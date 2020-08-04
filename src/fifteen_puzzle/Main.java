452f23b4812d68ac31a01a7d6ae7a941b61f64b9
SamuelSzurman <69159815+SamuelSzurman@users.noreply.github.com>
2020-08-04 14:58:59 +0200

    Update Main.java

--- src/fifteen_puzzle/Main.java
+++ src/fifteen_puzzle/Main.java
@@ -1,7 +1,7 @@
 package fifteen_puzzle;
 import javax.swing.*;
-public class Main {
 
+public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
