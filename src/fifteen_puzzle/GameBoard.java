5e53954588131de0d8716982064317410413ae7e
SamuelSzurman <SamuelSzurman@users.noreply.github.com>
2020-08-04 15:44:34 +0200

    minor update

--- src/fifteen_puzzle/GameBoard.java
+++ src/fifteen_puzzle/GameBoard.java
@@ -6,7 +6,6 @@ import javax.swing.*;
 import java.util.*;
 
 public class GameBoard extends JPanel{
-    //jakiś komentarz 
     //board
     final private int boardX = 0;
     final private int boardY = 100;
